47de9172509bd884266e1f1d94d283e630b7d9d3
chiaki-kjwr <chiakikajiwara@gmail.com>
2020-09-15 16:37:05 +0900

    importの記述を記載

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+// I need to import a helper LIbrary
+// From flutter to get contetnt on the screen
+// 　Flutterライブラリからインポートするために下記を記載する必要がある。
+import 'package:flutter/material.dart';
+// Define a main function to run whien our app starts
+
+//Create a new text widget to show some text on the screen
+
+//take the widget and get it on the screen
