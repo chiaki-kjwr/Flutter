b9b1f682b591ad80800a8efcc6d377bd7e87ce24
chiaki-kjwr <chiakikajiwara@gmail.com>
2020-09-16 13:53:44 +0900

    右下のボタンの表示を修正

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ void main() {
   var app = MaterialApp(
     home: Scaffold(
       floatingActionButton: FloatingActionButton(
-        child: Text('+'),
+        child: Icon(Icons.clear),
         onPressed: () {
         print('Hi There!');
       },
