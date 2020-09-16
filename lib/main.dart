221d8148a7898f8082f236477fc481aeb4ee675d
chiaki-kjwr <chiakikajiwara@gmail.com>
2020-09-16 11:20:59 +0900

    appを実行するソースコードを記載

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,13 @@
 // From flutter to get contetnt on the screen
 // 　Flutterライブラリからインポートするために下記を記載する必要がある。
 import 'package:flutter/material.dart';
+
 // Define a main function to run whien our app starts
-
-//Create a new text widget to show some text on the screen
-
+void main() {
+  //Create a new text widget to show some text on the screen
+  var app = MaterialApp(
+    home: Text('Hi there!'),
+  );
 //take the widget and get it on the screen
+  runApp(app);
+}
